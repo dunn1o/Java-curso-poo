0c2068fd7e3005d81ccbd2142ed591f9138257b3
Felipe Zerbinati <fezerba@gmail.com>
2026-04-17 15:47:57 -0300

    incluido scanner api

--- Java-curso-poo/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
+++ Java-curso-poo/src/br/com/alura/screenmatch/principal/PrincipalComBusca.java
@@ -6,12 +6,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.Scanner;
 
 public class PrincipalComBusca {
     public static void main(String[] args) throws IOException, InterruptedException {
+        Scanner leitura = new Scanner(System.in);
+        System.out.println("Digite um filme para busca: ");
+        var busca = leitura.nextLine();
+
+        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=1ae9f7fd";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-            .uri(URI.create("https://www.omdbapi.com/?t=matrix&apikey=1ae9f7fd"))
+            .uri(URI.create(endereco))
             .build();
         HttpResponse<String> response = client
             .send(request, BodyHandlers.ofString());
